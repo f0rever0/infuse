d74b864d199b31ace8aa98154afbbff2278b4687
f0ever0 <thddndud0830@gmail.com>
2024-06-09 18:45:37 +0900

    feat : 트위터 아이디 추가

--- src/components/main/Footer.tsx
+++ src/components/main/Footer.tsx
@@ -4,11 +4,11 @@ import icon_twitter from "@/assets/icons/icon_twitter.svg";
 
 export default function Footer() {
   return (
-    <div className="flex justify-end items-center pb-4">
+    <div className="flex justify-end items-center pb-4 pr-4">
       <Link href={"https://x.com/infuse_into_onf"}>
         <Image src={icon_twitter} alt="트위터 아이콘" width="20" height="20" />
       </Link>
-      <p className="text-light-gray pl-3 bold-14"> infuse_into</p>
+      <p className="text-light-gray pl-3 bold-14"> infuse_into_onf</p>
     </div>
   );
 }
