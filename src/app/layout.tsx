f1993c333d3319d7ce4f25f9afab86546ddd3932
f0ever0 <thddndud0830@gmail.com>
2024-06-09 16:28:45 +0900

    chore : 메타 태그 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,5 @@
-import type { Metadata } from "next";
 import "./globals.css";
 import { pretendard } from "@/utils/font";
-
-export const metadata: Metadata = {
-  title: "infuse",
-  description: "온앤오프 동영상 모음 페이지",
-};
 
 export default function RootLayout({
   children,
@@ -14,6 +8,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <title>infuse</title>
+        <meta name="description" content="온앤오프 동영상 모음 페이지" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icon.ico" />
+        <link rel="apple-touch-icon" href="/icon.ico"></link>
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body className={`${pretendard.variable} font-pretendard`}>
         {children}
       </body>
