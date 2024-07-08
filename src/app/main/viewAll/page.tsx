cdb87b4c657647f33a5538f2ecbcb8ede894bd2d
wooyoung <62867581+f0rever0@users.noreply.github.com>
2024-07-08 10:13:20 +0900

    chore : suspense 컴포넌트 분리

--- src/app/main/viewAll/page.tsx
+++ src/app/main/viewAll/page.tsx
@@ -8,7 +8,7 @@ import { translateLanguage } from "@/utils/translate";
 import Link from "next/link";
 import Image from "next/image";
 
-export default function ViewAllPage() {
+function VideoList() {
   // 어떤 비디오의 전체보기 페이지인지 앰플리튜드 심기
 
   const searchParams = useSearchParams();
@@ -20,32 +20,39 @@ export default function ViewAllPage() {
   );
 
   const videoList = video.find((item) => item.title === "mv");
+
+  return (
+    <section className="h-full mb-4">
+      <h2 className="text-white bold-24 py-4">{listTitle}</h2>
+      <div className="flex justify-center -ml-4">
+        <main className="grid grid-cols-2 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {videoList?.list.map((data: VideoData) => {
+            return (
+              <div
+                key={data.videoUrl}
+                className="border-slate-100 border-2 cursor-pointer w-[160px] h-[90px] sm:w-[240px] sm:h-[135px] md:w-[320px] md:h-[180px] lg:w-[360px] lg:h-[202.5px]"
+              >
+                <Link href={data.videoUrl}>
+                  {/* <Image
+              fill
+              src={data.sumnailUrl}
+              alt={`${data.videoTitle} 썸네일`}
+              title={data.videoTitle}
+            /> */}
+                </Link>
+              </div>
+            );
+          })}
+        </main>
+      </div>
+    </section>
+  );
+}
+
+export default function ViewAllPage() {
   return (
     <Suspense fallback={<p>Loading video...</p>}>
-      <section className="h-full mb-4">
-        <h2 className="text-white bold-24 py-4">{listTitle}</h2>
-        <div className="flex justify-center -ml-4">
-          <main className="grid grid-cols-2 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {videoList?.list.map((data: VideoData) => {
-              return (
-                <div
-                  key={data.videoUrl}
-                  className="border-slate-100 border-2 cursor-pointer w-[160px] h-[90px] sm:w-[240px] sm:h-[135px] md:w-[320px] md:h-[180px] lg:w-[360px] lg:h-[202.5px]"
-                >
-                  <Link href={data.videoUrl}>
-                    {/* <Image
-                  fill
-                  src={data.sumnailUrl}
-                  alt={`${data.videoTitle} 썸네일`}
-                  title={data.videoTitle}
-                /> */}
-                  </Link>
-                </div>
-              );
-            })}
-          </main>
-        </div>
-      </section>
+      <VideoList />
     </Suspense>
   );
 }
