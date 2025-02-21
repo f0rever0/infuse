c16b361ad1bc0f84efde514e10f3f292deeda5e4
wooyoung <thddndud0830@gmail.com>
2025-02-21 13:09:02 +0900

    feat : og description 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,8 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/icon.ico"></link>
         <meta name="theme-color" content="#000000" />
         <meta property="og:title" content="infuse" />
-        <meta property="og:type" content="온앤오프 동영상 모음 페이지" />
+        <meta property="og:description" content="온앤오프 동영상 모음 페이지" />
+        <meta property="og:type" content="website" />
         <meta property="og:image" content="/icon.ico" />
         <meta property="og:article:author" content="@infuse_into_onf" />
       </head>
