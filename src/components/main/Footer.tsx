abbf1466a3376ee2e73b53bfa4036fd8593b0f92
wooyoung <thddndud0830@gmail.com>
2025-03-23 10:33:39 +0900

    feat : footer 폰트 변경

--- src/components/main/Footer.tsx
+++ src/components/main/Footer.tsx
@@ -4,11 +4,11 @@ import icon_twitter from "@/assets/icons/icon_twitter.svg";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#121212] text-[#fff] text-lg flex justify-end items-center p-6">
+    <footer className="font-sans bg-[#121212] text-[#fff] text-sm flex justify-end items-center p-6">
       <Link href={"https://x.com/infuse_into_onf"}>
         {/* <Image src={icon_twitter} alt="트위터 아이콘" width="20" height="20" /> */}
       </Link>
-      <p className="text-[#f5f3ee]  pl-3 text-lg font-base">@infuse_into_onf</p>
+      <p className="text-[#f5f3ee]  pl-3 text-sm font-base">@infuse_into_onf</p>
     </footer>
   );
 }
